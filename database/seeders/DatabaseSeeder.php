007a9c130573b9fbfcb08a24ef4644b02b8295df
Ayelén Escalante <daianaayelenescalante@gmail.com>
2025-05-09 17:47:04 -0300

    segundo commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ServiciosSeeder::class);
-        $this->call(ClienteSeeder::class);
+        //$this->call(ClienteSeeder::class);
     }
 
     
